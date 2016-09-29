3efab9268dab11610b5eb97669c75c384b34fb4a
Lucas Bordignon <lucaspbordignon99@gmail.com>
2016-09-29 13:41:55 -0300

    Small improvements. Client class

--- client.cpp
+++ client.cpp
@@ -5,35 +5,26 @@ Client::Client(double arrivalTime, double clientsQueueTime, int cashierEfficienc
 {
     departureTime_ = clientsQueueTime + purchaseTime(cashierEfficiency);
     arrivalTime_ = arrivalTime;
-    totalItemsValue_ = totalItemsValue();
+    totalItemsValue_ = allProductsCost();
     payWithMoney_ = paymentMethod();
     searchSmallestQueue_ = queueChoice();
     items_ = (rand()*99) + 2;
 }
 
-//! Returns the 'paymant method' to a client.
-/*!
- *  Just return 'true' if the client will pay with money and 'false'if he will
- *pay with check, with a percentage of 80 to pay with money.
- */
+
 bool Client::paymentMethod()
 {
     if (std::rand() < 0.20) return false;
     return true;
 }
 
-//! Returns the queue choice criterion.
-/*!
- *  Returns 'true' if the client choose the queue by the number of people in,
- *and 'false' if the client choose the queue by the items number.
- */
 bool Client::queueChoice()
 {
     if (std::rand() < 0.50) return false;
     return true;
 }
 
-double Client::totalItemsValue()
+double Client::allProductsCost()
 {
     double value = 0.0;
     for (auto i = 0; i < items_; ++i) {
--- client.hpp
+++ client.hpp
@@ -2,9 +2,21 @@ class Client
 {
     public:
         Client(double, double, int);
+
+        //! Returns the 'paymant method' to a client.
+        /*!
+         *  Just return 'true' if the client will pay with money and 'false'if he will
+         *pay with check, with a percentage of 80 to pay with money.
+         */
         bool paymentMethod();
+
+        //! Returns the queue choice criterion.
+        /*!
+         *  Returns 'true' if the client choose the queue by the number of people in,
+         *and 'false' if the client choose the queue by the items number.
+         */
         bool queueChoice();
-        double totalItemsValue();
+        double allProductsCost();
         double purchaseTime(int);
 
     private:
