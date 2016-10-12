e35b211d2f55a488e5224a140943ef9bcf6ce3d7
Lucas Bordignon <lucaspbordignon99@gmail.com>
2016-10-12 01:00:19 -0300

    Parameters read through the 'input_parameters' file

--- main.cpp
+++ main.cpp
@@ -1,14 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
 #include "grocery_store.hpp"
+
+std::vector<std::string> readInputFile();
 
 int main() {
     structures::CircularList<Cashier> cashiers;
-    Cashier joao(1, 800, "joao");
-    Cashier maria(1, 800, "maria");
-    cashiers.push_front(joao);
-    cashiers.push_front(maria);
-    GroceryStore store(3600, 10, std::move(cashiers));
+
+    std::vector<std::string> parameters = readInputFile();
+    for (auto i = 0; i < std::stoi(parameters[3]); ++i) {
+        std::stringstream read_line(parameters[i+4]);
+        std::string cashier_name;
+        int salary, efficiency;
+        read_line >> cashier_name >> efficiency >> salary;
+        std::cout << efficiency << "\n";
+        Cashier newCashier(efficiency, salary, cashier_name);
+        cashiers.push_front(newCashier);
+    }
+
+    GroceryStore store(std::stoi(parameters[1])*3600, std::stoi(parameters[2]), std::move(cashiers));
     store.simulate();
     store.showResults();
 }
 
+std::vector<std::string> readInputFile() {
+    auto output_vector = std::vector<std::string>{};
+    std::ifstream file;
+    file.open("input_parameters.txt");
+    while (!file.eof()) {
+        char read_line[80];
+        file.getline(read_line, 80);
+        if (read_line[0] != '#')
+        {
+           output_vector.push_back(read_line);
+        }
+    }
+    return output_vector;
+}
